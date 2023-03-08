f73918bb05132b912d95e335a2d73e5eea1005d5
Irena Ijacic <irenaijacic17@gmail.com>
2023-03-08 13:03:50 +0100

    Brisanje komentara

--- OTS_Cas1/Classes/MassConverter.cs
+++ OTS_Cas1/Classes/MassConverter.cs
@@ -13,7 +13,7 @@ namespace OTS_Cas1
              return Math.Round(value / 2.205, 3);
         }
         
-        //Komentar1
+        
         public double ConvertArray(string array)
         {
             string[] values = array.Split(',');
